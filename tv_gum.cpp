9db0d853e550ae11bd74c42fa29960e3f5adf7b6
Garik <garikghazaryan1983@gmail.com>
2023-03-02 23:46:41 +0400

    tv_gum by function

--- tv_gum.cpp
+++ tv_gum.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 #include <cmath>
 
+long get_mun_count(long);
+void get_sum(long,long);
 
 int main(){
-int n=20;
-int i;
-int num,a,aa,x,sum;
-
-std::cin >> num;
-
-	for (i=1;i<=n;i++)
-		{
-		x=pow(10,(n-i));
-		a=num/x;
-		aa=num%x;
-		num=aa;
-		sum=sum+a;
 	
+	long num,sum,n;
+	std::cin >> num;
+	
+	n=get_mun_count(num);
+	get_sum(n,num);
+}
+
+
+long get_mun_count(long num)
+{
+	long n=0;
+	while (true)
+	{
+		n++;
+		long z=num/pow(10,n);
+		if (z==0)
+		{
+			break;
+		}
+	}
+
+	return n;
+}
+
+
+void get_sum(long n, long num)
+{
+	long sum=0;
+	for (long i=1;i<=n;i++)
+		{
+		long x=pow(10,(n-i));
+		long a=num/x;
+		long aa=num%x;
+		num=aa;
+	        sum=sum+a;
 		}
 
-std::cout << sum <<std::endl;
+std::cout << "sum=  "<< sum <<std::endl;
+
 }
+
+
