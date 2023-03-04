7e5f1063f2e29f7ebb8e291ee5b4fdbb3d2d6b96
Garik <garikghazaryan1983@gmail.com>
2023-03-04 21:55:47 +0400

    modify not_full.cpp to -full

--- not_full.cpp
+++ not_full.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
+
 int main()
 {
 	
-	std::string nstr = "";
 	std::string mstr = "";
 	std::getline (std::cin,mstr);
-	int count=0;
-	std::string tstr = "";
-	int i=0;
-	while (mstr[i]!=' ')
-	{
-		nstr+=mstr[i];
-		i++;	
-	}
-		tstr=nstr;
-		count=i;
+	std::string bar = "";
+	std::string erk_bar = "";
+	
+    for (int i =0;i<mstr.size();i++) {
 
-std::cout<<tstr<<std::endl;
-std::cout<<count<<std::endl;
+        if (mstr[i] == ' ') {
+
+            if (bar.size() > erk_bar.size()) {
+
+                erk_bar=bar;
+            }
+          	bar = "";
+
+        } else {
+
+            bar+= mstr[i];
+        }
+    }
+
+  std::cout << erk_bar <<std::endl;
+
 }
+
+
+
